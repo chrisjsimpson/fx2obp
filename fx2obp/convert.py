0354454ac34e2e7a39cd53fffd184d4e7829d7e4
chrisjsimpson <chris@karmacomputing.co.uk>
2019-08-26 13:56:39 +0100

    refactor run() to check env vars upon run. TODO do preflight check for envars and fail with helpful error

--- fx2obp/convert.py
+++ fx2obp/convert.py
@@ -4,52 +4,51 @@ import os
 from datetime import datetime
 from pathlib import Path
 
-# If you want to post, set `export POST_TO_OBP=true` before running script
-POST_TO_OBP=os.getenv('POST_TO_OBP', '')
-# If you want to write to file, (for example testing) set `export WRITE_TO_FILE=true` before running script
-WRITE_TO_FILE=os.getenv('WRITE_TO_FILE', '')
-POST_URL="{}/obp/v3.1.0/banks/{}/fx"
-AUTH_TOKEN=os.getenv('AUTH_TOKEN')
-API_HOST=os.getenv('API_HOST')
-BANK_ID=os.getenv('BANK_ID')
-CURRENCIES=os.getenv('CURRENCIES') # Only get specified currencies: e.g. export CURRENCIES=usd,gbp,dzd
-
-# Get bank ids
-req = requests.get(API_HOST + '/obp/v3.1.0/banks', headers={'Content-Type':'Application/Json'})
-if req.status_code != 200:
-  print(req)
-banks = json.loads(req.text)['banks']
-
-'''
-For each file input , convert it to an OBP foriegn exchange valid payload.
-Populate for each bank.
-
-Required roles:
-- CanCreateFxRate
-- CanCreateFxRateAtAnyBank
-
-Valid example:
-{
-  "bank_id":"bankid123",
-  "from_currency_code":"EUR",
-  "to_currency_code":"USD",
-  "conversion_value":1.0,
-  "inverse_conversion_value":1.0,
-  "effective_date":"2017-09-19T00:00:00Z"
-}
-Invalid format example from source: (http://www.floatrates.com)
-"usd": {
-  "code": "USD",
-  "alphaCode": "USD",
-  "numericCode": "840",
-  "name": "U.S. Dollar",
-  "rate": 1.0001337101383,
-  "date": "Wed, 20 Mar 2019 12:00:02 GMT",
-  "inverseRate": 0.9998663077377
-}
-'''
-
 def run():
+  # If you want to post, set `export POST_TO_OBP=true` before running script
+  POST_TO_OBP=os.getenv('POST_TO_OBP', '')
+  # If you want to write to file, (for example testing) set `export WRITE_TO_FILE=true` before running script
+  WRITE_TO_FILE=os.getenv('WRITE_TO_FILE', '')
+  POST_URL="{}/obp/v3.1.0/banks/{}/fx"
+  AUTH_TOKEN=os.getenv('AUTH_TOKEN')
+  API_HOST=os.getenv('API_HOST')
+  BANK_ID=os.getenv('BANK_ID')
+  CURRENCIES=os.getenv('CURRENCIES') # Only get specified currencies: e.g. export CURRENCIES=usd,gbp,dzd
+
+  # Get bank ids
+  req = requests.get(API_HOST + '/obp/v3.1.0/banks', headers={'Content-Type':'Application/Json'})
+  if req.status_code != 200:
+    print(req)
+  banks = json.loads(req.text)['banks']
+
+  '''
+  For each file input , convert it to an OBP foriegn exchange valid payload.
+  Populate for each bank.
+
+  Required roles:
+  - CanCreateFxRate
+  - CanCreateFxRateAtAnyBank
+
+  Valid example:
+  {
+    "bank_id":"bankid123",
+    "from_currency_code":"EUR",
+    "to_currency_code":"USD",
+    "conversion_value":1.0,
+    "inverse_conversion_value":1.0,
+    "effective_date":"2017-09-19T00:00:00Z"
+  }
+  Invalid format example from source: (http://www.floatrates.com)
+  "usd": {
+    "code": "USD",
+    "alphaCode": "USD",
+    "numericCode": "840",
+    "name": "U.S. Dollar",
+    "rate": 1.0001337101383,
+    "date": "Wed, 20 Mar 2019 12:00:02 GMT",
+    "inverseRate": 0.9998663077377
+  }
+  '''
   def post_currency(fh):
     data = fh.read()
     src_currency = str(currency_file.resolve()).split('currencies/')[1].split('.json')[0]
