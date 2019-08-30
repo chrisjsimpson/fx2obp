8095d9e94665b80529b8c473feab056c7fc3d299
chrisjsimpson <chris@karmacomputing.co.uk>
2019-08-30 14:17:46 +0100

    allow ephemeral fetching of latest currencies (do not store long term on disk, ust /tmp)

--- fx2obp/fetch_currencies.py
+++ fx2obp/fetch_currencies.py
@@ -4,15 +4,19 @@ from time import sleep
 from pathlib import Path
 import tempfile
 
-def fetch_currencies(useTmpDir=False, ephemeral=False, **kwargs):
+def fetch_currencies(ephemeral=False, **kwargs):
     """ Download every new foreign exchange rate available 
         
-        :param useTmpDir: Use a tempfs, don't persist. If set, ephemeral
-                          must also be True
-        :param ephemeral: As soon as all currencies have beed fetched, attempt
-                          to post directly to an sandbox using postFx. This is 
-                          needed because python tempfile/dirs are automatically
-                          deleted after completion of the tempfule context.
+        :param ephemeral: Use a tempfs, don't persist to disk
+                          as soon as all currencies have beed fetched, attempt
+                          to post directly to an sandbox using postFx.
+                          NOTE: this client code is also responsible for 
+                          deleting the temporary directory and its contents 
+                          when done with it as we use mkdtemp see
+                          https://docs.python.org/3/library/tempfile.html
+        :param sourceDir: As kwarg, may be used to override default folder
+                          from which currencies are loaded (default is
+                          './currencies' of the current working directory)
         :return: Path to currencies directory (either tmp or persistant)
     """
 
@@ -27,8 +31,13 @@ def fetch_currencies(useTmpDir=False, ephemeral=False, **kwargs):
                 download_links.append(link["href"])
         except:
             pass
-    if useTmpDir: # Prepare temp directory to store currencies & exchange rates
-      tmpdir = tempfile.TemporaryDirectory()
+    if ephemeral: # Prepare temp directory to store currencies & exchange rates
+      path = tempfile.mkdtemp()
+    else:
+      try:
+        path = kwargs['sourceDir']
+      except KeyError:
+        path = './currencies'
 
     # Download each currency conversion into list
     currencies = {}
@@ -40,11 +49,9 @@ def fetch_currencies(useTmpDir=False, ephemeral=False, **kwargs):
             currencies[currency_code] = resp.text
             print("Fetching {}".format(currency_code))
             # Write to file or tmpdir
-            if useTmpDir:
-              path = tmpdir.name
-              filePath = Path(tmpdir.name, '{}.json'.format(currency_code))
+            if ephemeral:
+              filePath = Path(path, '{}.json'.format(currency_code))
             else: 
-              path = "./currencies"
               filePath = Path("./currencies/{}.json".format(currency_code))
             with open(filePath, "w") as f:
                 f.write(resp.text)
